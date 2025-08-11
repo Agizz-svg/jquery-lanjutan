34d6fdc3a03b1164fe6648c10b4da50cd8a7b2cf
Agizz-svg <Argies2007>
2025-08-11 13:45:35 +0700

    modaldialog commit

---
+++ modaldialog.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#tombol').click(function(){
+        $('#modal-kotak , #bg').fadeIn("slow");
+    });
+    $('#tombol-tutup').click(function(){
+        $('#modal-kotak , #bg').fadeOut("slow");
+    });
+});
